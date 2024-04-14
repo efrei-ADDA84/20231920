b300354c7b672f6ff72ac3232c08165f15fa7460
Rami-lwz <78386345+Rami-lwz@users.noreply.github.com>
2024-04-14 16:22:46 +0200

    Ceci marche

--- TP2/api/app.py
+++ TP2/api/app.py
@@ -1,22 +1,21 @@
 from flask import Flask, request, jsonify
 import requests
 import os
-import argparse
 
 app = Flask(__name__)
 
-@app.route('/weather', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_weather():
-    api_key = request.args.get('apikey')
+    api_key = os.environ.get('API_KEY')  # Get the API key from an environment variable
     lat = request.args.get('lat')
     lon = request.args.get('lon')
+    
+    if not api_key:
+        return jsonify({"error": "API key is missing"}), 401  # Return an error if the API key isn't provided
+    
     url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
     response = requests.get(url)
-    print(api_key, lat, lon)
-    
     return jsonify(response.json())
 
 if __name__ == '__main__':
-    # app on port 8081
-    app.run(debug=True, port=8081)
-            
+    app.run(debug=True, host="0.0.0.0",port=8081)
