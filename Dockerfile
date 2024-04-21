e4effd8f38b28d088de36493cc10f64dae4df9c2
Rami-lwz <78386345+Rami-lwz@users.noreply.github.com>
2024-04-21 15:15:03 +0200

    test api et variables tf

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.11.9-slim
+
+WORKDIR /usr/src/app
+
+COPY ./api/requirements.txt .
+
+COPY ./api/app.py .
+
+
+EXPOSE 8081
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+CMD ["python", "./app.py"]
---
+++ TP4/variables.tf
@@ -0,0 +1,21 @@
+variable "azure_subscription_id" {
+  default = "ab108b80-104a-443a-ac5f-6b896aa16313"
+}
+variable "user_admin" {
+  default = "devops"
+}
+variable "location" {
+  default = "france central"
+}
+variable "network_name" {
+  default = "network-tp4"
+}
+variable "subnet" {
+  default = "internal"
+}
+variable "azure_resource_group_name" {
+  default = "ADDA84-CTP"
+}
+variable "azure_vm_name" {
+  default = "devops-20231920"
+}
